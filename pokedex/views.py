d0816e88fdff486c6f6d7e4856fcd24d817f7c8c
Keila Cerda <keila.cerda@uisek.edu.ec>
2025-11-12 15:12:09 -0500

    Finalización de laboratorio 3

---
+++ pokedex/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def index(request):
+    pokemons = ['Pikachu', 'Charmander', 'Bulbasaur', 'Squirtle']
+    return render(request, 'index.html', {
+        'pokemons': pokemons
+    })
+
+def pokemon_details(request, pokemon):
+    return render(request, 'pokemon_details.html', {
+        'pokemon' : pokemon
+    } )
